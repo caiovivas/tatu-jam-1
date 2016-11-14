b79324660cd413f18e7256bae8bda184dd962b75
Caio Vivas <caionvivas@yahoo.com.br>
2016-11-14 17:03:20 -0200

    Model do porquinho adicionado

--- Assets/Scripts/AIEntity.cs
+++ Assets/Scripts/AIEntity.cs
@@ -15,24 +15,49 @@ public class AIEntity : MonoBehaviour {
 	public Vector3[] patrolNodes;
 	public bool randomPatrolOrder;
 	public Vector3 lastSeenPlayerPosition;
+	public enum DetectionStatus{
+		UNDETECTED,
+		SUSPICIOUS,
+		DETECTED
+	}
+	public DetectionStatus currentDetectionStatus;
 
 
 	//Runtime
+	public NavMeshAgent agent;
 	float activeActionTimer;
+	bool moving;
+	int currentPatrolNodeIndex;
 
 	void Update(){
 		ThinkCycle ();
+
+		if (Vector3.Distance (transform.position, agent.destination) <= 0.3f) {
+			moving = false;
+		}
 	}
 
 	public void ThinkCycle(){
 		activeActionTimer -= Time.deltaTime;
+
 		if (currentAction == Action.IDLE) {
 			if (activeActionTimer <= 0) {
-				
+				if (currentDetectionStatus == DetectionStatus.UNDETECTED) {
+				}
+				if (currentDetectionStatus == DetectionStatus.SUSPICIOUS) {
+				}
+				if (currentDetectionStatus == DetectionStatus.DETECTED) {
+				}
 			}
-
 		}
 
+	}
+
+	void GoTo(Vector3 loc){
+		agent.destination = loc;
+	}
+
+	Vector3 nextPatrolNode(){
 	}
 
 
