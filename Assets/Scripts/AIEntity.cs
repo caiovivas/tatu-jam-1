fba37990486d0898b9983fb2a3cb4f7e859288cf
Caio Vivas <caionvivas@yahoo.com.br>
2016-11-14 16:49:57 -0200

    Criacao de diretorios

---
+++ Assets/Scripts/AIEntity.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIEntity : MonoBehaviour {
+	public enum Action{
+		PATROL,
+		INVESTIGATE,
+		HUNT,
+		IDLE,
+		STUNNED
+	}
+	public Action currentAction;
+	public float idleTime;
+	public bool idleTimeRandom;
+	public Vector3[] patrolNodes;
+	public bool randomPatrolOrder;
+	public Vector3 lastSeenPlayerPosition;
+
+
+	//Runtime
+	float activeActionTimer;
+
+	void Update(){
+		ThinkCycle ();
+	}
+
+	public void ThinkCycle(){
+		activeActionTimer -= Time.deltaTime;
+		if (currentAction == Action.IDLE) {
+			if (activeActionTimer <= 0) {
+				
+			}
+
+		}
+
+	}
+
+
+
+}
