2249f03f4c19387e362cf891d3ca525ddc8899ea
Andre Luiz Rodrigues <andluizlima@iMac-de-Andre.local>
2016-11-15 16:07:10 -0200

    Level 1 atualizado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour {
 
 	public Rigidbody rb;
 	public Animator anim;
-	public float maxSpeed;
+	public float maxSpeed, maxSpeedH;
 	public float rot;
 
 	void Start(){
@@ -13,17 +13,19 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void Update(){
+		maxSpeed = 7;
+		maxSpeedH = 2;
 		rot += Input.GetAxisRaw ("Horizontal") * Time.deltaTime * 15f;
-		if(rot < 0 && Input.GetAxisRaw ("Horizontal") >= -0.5 && Input.GetAxisRaw ("Horizontal") <= 0.5)
+		if(rot < 0 && Input.GetAxisRaw ("Horizontal") >= -0.5 && Input.GetAxisRaw ("Horizontal") < 1)
 			rot += Time.deltaTime*15f;
-		if(rot > 0 && Input.GetAxisRaw ("Horizontal") >= -0.5 && Input.GetAxisRaw ("Horizontal") <= 0.5)
+		if(rot > 0 && Input.GetAxisRaw ("Horizontal") <= 0.5 && Input.GetAxisRaw ("Horizontal") > -1)
 			rot -= Time.deltaTime*15f;
-		if (rot < -2) {
-			rot = -2;
-		} else if (rot > 2) {
-			rot = 2;
+		if (rot < -maxSpeedH) {
+			rot = -maxSpeedH;
+		} else if (rot > maxSpeedH) {
+			rot = maxSpeedH;
 		}
-		transform.RotateAround (transform.position, Vector3.up, rot * 2);
+		transform.RotateAround (transform.position, Vector3.up, rot * maxSpeedH);
 		anim.SetFloat("speed", rb.velocity.magnitude);
 		if (rb.velocity.magnitude <= maxSpeed) {
 			rb.AddForce (transform.forward * Time.deltaTime * 1000f * Input.GetAxis ("Vertical"));
